6066053d12aa0b39436daab53d945f72e456ea84
kyminbb <kyminbb.kim@gmail.com>
2019-09-07 18:36:33 -0400

    Removed video-stream-sample

---
+++ react-backend/client/src/upload.js
@@ -0,0 +1,25 @@
+// Imports the Google Cloud client library
+const {Storage} = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+
+const bucketName = 'unight_uploaded';
+const filename = './download.jpeg';
+
+async function upload() {
+  // Uploads a local file to the bucket
+  await storage.bucket(bucketName).upload(filename, {
+    // Support for HTTP requests made with `Accept-Encoding: gzip`
+    gzip: true,
+    metadata: {
+      // Enable long-lived HTTP caching headers
+      // Use only if the contents of the file will never change
+      // (If the contents will change, use cacheControl: 'no-cache')
+      cacheControl: 'public, max-age=31536000',
+    },
+  });
+  console.log(`${filename} uploaded to ${bucketName}.`);
+};
+
+upload();
