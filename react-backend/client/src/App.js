1849895815495a493456eaf71b470e1a8939d48b
JP Park <jaep3347@gmail.com>
2019-09-07 18:02:02 -0400

    added upload feature

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import {DropzoneArea} from 'material-ui-dropzone'
+import NavBar from './components/NavBar'
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+
   state = {users: []}
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+ componentDidMount() { //example of fetching json data
+   fetch('/users')
+     .then(res => res.json())
+     .then(users => this.setState({ users }));
+ }
+
+ constructor(props){  //example of showing state when uploading image
+     super(props);
+     this.state = {
+       files: []
+     };
+   }
+   handleChange(files){
+     this.setState({
+       files: files
+     });
+   }
+
 
   render() {
     return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+      <div classname="App"> //wrapper for material ui
+        <NavBar /> //blue navibar
+        <DropzoneArea  //zone where you upload
+        onChange={this.handleChange.bind(this)}
+        />
       </div>
-    );
+
+    )
   }
+
 }
 
-export default App;
+
+export default App
