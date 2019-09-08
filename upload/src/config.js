a6aac9b9d49be32e7d4fdef6c3b245ca45427709
kyminbb <kyminbb.kim@gmail.com>
2019-09-08 04:46:21 -0400

    Jebal JJJJJJOM

--- upload/src/config.js
+++
@@ -1,6 +0,0 @@
-export const Config = {
-  bucketName: 'unight-uploaded',
-  region: 'us-east-2',
-  accessKeyId: 'AKIAJAKZ7R766R6B2S3Q',
-  secretAccessKey: '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'
-}
